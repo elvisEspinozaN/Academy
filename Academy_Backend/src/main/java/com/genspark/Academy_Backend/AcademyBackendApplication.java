40684daaef68d6098921b3dfc7055ff75e66f01f
elvisEspinoza <elvis_espinoza98@yahoo.com>
2022-08-15 21:52:43 -0400

    wip: added backend files

---
+++ Academy_Backend/src/main/java/com/genspark/Academy_Backend/AcademyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.Academy_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyBackendApplication.class, args);
+	}
+
+}
---
+++ Academy_Backend/src/test/java/com/genspark/Academy_Backend/AcademyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.Academy_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
